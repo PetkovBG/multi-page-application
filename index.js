061617c4351bae0f27f04e67c4f87b551003ffff
PetkovBG <97334281+PetkovBG@users.noreply.github.com>
2023-02-13 14:31:18 +0200

    Setting up view engine in index file

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 
+const handlebars = require('express-handlebars');
+
 const port = 3500;
 
 const routes = require('./routes');
 
 const app = express();
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+
+app.set('view engine', 'hbs');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
