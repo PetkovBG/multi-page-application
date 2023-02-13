c7080998a34d1b6525a630f1a354157dc2672abe
PetkovBG <97334281+PetkovBG@users.noreply.github.com>
2023-02-13 15:26:42 +0200

    added User model

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,14 @@ const router = require('express').Router();
 
 router.get('/register', (req, res) => {
     res.render('auth/register');
+});
+
+router.post('/register', (req, res) => {
+        const { username, email, password, repeatPassword } = req.body;
+
+        
+    
+    res.render('/')
 })
 
 module.exports = router;
