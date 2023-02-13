687f2963baa5b80891fb34b56dbe5cc219e87720
PetkovBG <97334281+PetkovBG@users.noreply.github.com>
2023-02-13 15:00:51 +0200

    Setting up the database - MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const handlebars = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const port = 3500;
 
@@ -18,5 +19,8 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
 
 app.use(routes);
+
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb://127.0.0.1:27017/mpa');
 
 app.listen(port, () => console.log(`Server has started on port #${port}..`));
